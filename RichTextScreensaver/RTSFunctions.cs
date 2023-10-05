88016ab7433473528551f91ec99c150c8722374b
LuckyLars32 <94763457+LuckyLars32@users.noreply.github.com>
2023-10-05 22:49:03 +0200

    v2.1 - fixed incorrect logfile location
    I had the logfile path hardcoded for testing and forgot about that. 
    Now the logfile is located at %LOCALAPPDATA%\RichTextScreensaver\TSLog.txt .
    If the logfile or the directory itself does not exist, the program tries to create it.

--- RichTextScreensaver/RTSFunctions.cs
+++ RichTextScreensaver/RTSFunctions.cs
@@ -45,20 +45,56 @@ namespace RichTextScreensaver
         //Log ERRORs to a Logfile
         public static void WriteToLog(string logMsg, string logLevel)
         {
-            string LogfilePath = "C:\\Scripte\\TSLog.txt";
-            System.IO.FileStream stream = System.IO.File.Open(LogfilePath, System.IO.FileMode.Append);
-            if (!stream.CanWrite)
+            //get Localaappdata Path (Usually: C:\Users\%USERNAME%\AppData\Local)
+            string LocalAppDataPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
+            //Full Logfile path
+            string LogfilePath = LocalAppDataPath + "\\RichTextScreensaver\\TSLog.txt";
+            //Check if the file exists, if not check if the directory exists 
+            if (System.IO.File.Exists(LogfilePath) == false)
             {
-                System.Windows.Forms.MessageBox.Show("TextScreensaver can't write to Logfile", "FatalERROR");
+                try
+                {
+                    // Check if the directory exists 
+                    if (System.IO.Directory.Exists(LocalAppDataPath + "\\RichTextScreensaver") == false)
+                    {
+                        try
+                        {
+                            //If the directory does not exist, try to create the directory.
+                            System.IO.Directory.CreateDirectory(LocalAppDataPath + "\\RichTextScreensaver");
+                        }
+                        //Catch exeption if directory cannot be created for whatever reason, show error message as messagebox and close RichTextScreensaver.
+                        catch (Exception e)
+                        {
+                            System.Windows.Forms.MessageBox.Show("RichTextScreensaver can't create Logfile directory", "FatalERROR");
+                            System.Windows.Forms.Application.Exit();
+                        }
+                    }
+                    //If the file does not exist, try to create the file.
+                    System.IO.FileStream createFileStream = System.IO.File.Create(LogfilePath);
+                    createFileStream.Close();
+                }
+                //Catch exeption if file cannot be created for whatever reason, show error message as messagebox and close RichTextScreensaver.
+                catch (Exception e)
+                {
+                    System.Windows.Forms.MessageBox.Show("RichTextScreensaver can't create Logfile", "FatalERROR");
+                    System.Windows.Forms.Application.Exit();
+                }
             }
-            else
-            {
-                logMsg.Replace("\r\n", "").Replace("\n", ""); //Remove new line swich if contained. We want each log message to be one line only!
-                System.IO.StreamWriter writer = new System.IO.StreamWriter(stream);
-                string LogMessage = logLevel + " " + logMsg + '\n';
-                writer.WriteLine(LogMessage);
-                writer.Close();
-            }
+            //Actually write the message to the logfile
+                System.IO.FileStream stream = System.IO.File.Open(LogfilePath, System.IO.FileMode.Append);
+                if (!stream.CanWrite)
+                {
+                    System.Windows.Forms.MessageBox.Show("Rich TextScreensaver can't write to Logfile", "FatalERROR");
+                }
+                else
+                {
+                    logMsg.Replace("\r\n", "").Replace("\n", ""); //Remove new line swich if contained. We want each log message to be one line only!
+                    System.IO.StreamWriter writer = new System.IO.StreamWriter(stream);
+                    string LogMessage = logLevel + " " + logMsg + '\n';
+                    writer.WriteLine(LogMessage);
+                    writer.Close();
+                }
+
         }
     }
 }
